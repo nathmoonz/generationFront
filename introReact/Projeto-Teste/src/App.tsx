baaee5f32c6a2def7cd7a0be8fc74bb325f3819b
Teste teste <nathmoonz@hotmail.com>
2024-02-14 11:46:20 -0300

    projeto introdução a react creado

---
+++ introReact/Projeto-Teste/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Home from "./paginas/home/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ introReact/Projeto-Teste/src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Manipulando o mundo</h1>
+      <img
+        className="img"
+        src="https://media.istockphoto.com/id/183269098/pt/foto/gato-e-o-mundo.webp?b=1&s=170667a&w=0&k=20&c=CUq-leIljwiTbUdHKZT5be7BkKfrLGsWn1Gf9zq9aUM="
+        alt="Gato irritado"
+      />
+    </>
+  );
+}
+
+export default Home;
